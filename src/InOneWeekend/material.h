2362056e8552a63a9ca7f4836487b4adae749872
LKR <grleecode@gmail.com>
2026-04-08 00:33:00 +0900

    Update ray tracing study code

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -78,17 +78,20 @@ class dielectric : public material {
 
         vec3 unit_direction = unit_vector(r_in.direction());
         double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
-        double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+        double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+        if (cannot_refract||reflectance(cos_theta, ri) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, ri);
 
         scattered = ray(rec.p, direction);
+        vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, refracted);
         return true;
     }
 
--- src/InOneWeekend/vec3.h
+++ src/InOneWeekend/vec3.h
@@ -1,22 +1,13 @@
 #ifndef VEC3_H
 #define VEC3_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
+
 
 class vec3 {
-  public:
+public:
     double e[3];
 
-    vec3() : e{0,0,0} {}
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    vec3() : e{ 0,0,0 } {}
+    vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
 
     double x() const { return e[0]; }
     double y() const { return e[1]; }
@@ -41,7 +32,7 @@ class vec3 {
     }
 
     vec3& operator/=(double t) {
-        return *this *= 1/t;
+        return *this *= 1 / t;
     }
 
     double length() const {
@@ -49,11 +40,10 @@ class vec3 {
     }
 
     double length_squared() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 
     bool near_zero() const {
-        // Return true if the vector is close to zero in all dimensions.
         auto s = 1e-8;
         return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
     }
@@ -63,7 +53,7 @@ class vec3 {
     }
 
     static vec3 random(double min, double max) {
-        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 };
 
@@ -72,6 +62,10 @@ using point3 = vec3;
 
 
 // Vector Utility Functions
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
 
 inline vec3 operator+(const vec3& u, const vec3& v) {
     return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
@@ -86,7 +80,7 @@ inline vec3 operator*(const vec3& u, const vec3& v) {
 }
 
 inline vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
 inline vec3 operator*(const vec3& v, double t) {
@@ -94,19 +88,19 @@ inline vec3 operator*(const vec3& v, double t) {
 }
 
 inline vec3 operator/(const vec3& v, double t) {
-    return (1/t) * v;
+    return (1 / t) * v;
 }
 
 inline double dot(const vec3& u, const vec3& v) {
     return u.e[0] * v.e[0]
-         + u.e[1] * v.e[1]
-         + u.e[2] * v.e[2];
+        + u.e[1] * v.e[1]
+        + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(const vec3& v) {
@@ -114,8 +108,9 @@ inline vec3 unit_vector(const vec3& v) {
 }
 
 inline vec3 random_in_unit_disk() {
-    while (true) {
-        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+    while (true)
+    {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
         if (p.length_squared() < 1)
             return p;
     }
@@ -123,31 +118,31 @@ inline vec3 random_in_unit_disk() {
 
 inline vec3 random_unit_vector() {
     while (true) {
-        auto p = vec3::random(-1,1);
+        auto p = vec3::random(-1, 1);
         auto lensq = p.length_squared();
-        if (1e-160 < lensq && lensq <= 1.0)
-            return p / sqrt(lensq);
+        if (1e-160 < lensq && lensq <= 1.0) {
+            return p / std::sqrt(lensq);
+        }
     }
 }
 
 inline vec3 random_on_hemisphere(const vec3& normal) {
     vec3 on_unit_sphere = random_unit_vector();
-    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+    if (dot(on_unit_sphere, normal) > 0.0) {
         return on_unit_sphere;
-    else
-        return -on_unit_sphere;
+    }
+    return -on_unit_sphere;
 }
 
 inline vec3 reflect(const vec3& v, const vec3& n) {
-    return v - 2*dot(v,n)*n;
+    return v - 2 * dot(v, n) * n;
 }
 
 inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
     auto cos_theta = std::fmin(dot(-uv, n), 1.0);
-    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
     vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
     return r_out_perp + r_out_parallel;
 }
-
 
 #endif
